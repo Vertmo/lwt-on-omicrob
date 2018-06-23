5b8063d42f2b92ccf90fc8a759bbd75e9c7e3932
Vertmo <basile.pesin@gmail.com>
2018-06-23 10:52:57 +0200

    Since the update to OMicroB, it works (thanks to Steven Varoumas)

--- examples/basic/basic.ml
+++ examples/basic/basic.ml
@@ -1,10 +1,7 @@
-(*let calcul = fun n ->
-  Lwt.return ((n + 1) * 2)*)
-
 let calcul n = Lwt.return (n + 1)
 
-(* let choice = fun () ->
- *   (Lwt.choose [Lwt.return 1; Lwt.return 2]) *)
+let choice () =
+  (Lwt.choose [Lwt.return 1; Lwt.return 2])
 
 let () = ()
   (*(choice ()) >>= calcul >>= (fun n -> (Lwt.return ()))*)
