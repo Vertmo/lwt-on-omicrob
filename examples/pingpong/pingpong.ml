24fe8f3f13f0d5ae0b58dd86bed4d06202d33e98
Vertmo <basile.pesin@gmail.com>
2018-07-04 11:34:02 +0200

    Added pingpong example

---
+++ examples/pingpong/pingpong.ml
@@ -0,0 +1,17 @@
+open Avr
+open Lwt
+
+let r = PIN24 and g = PIN22
+
+let activatePins () =
+  Lwt.return (
+    pin_mode r OUTPUT;
+    pin_mode g OUTPUT;
+  )
+
+let rec pingpong () =
+  Lwt.return (digital_write r HIGH; delay 1000; digital_write r LOW) >>=
+  (fun () -> Lwt.return (digital_write g HIGH; delay 1000; digital_write g LOW) >>= pingpong)
+
+let () =
+  Lwt_main.run (activatePins () >>= pingpong)
